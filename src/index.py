f6a2b4918c5410558f204ee53a9b1dcd47bcd6af
Ssoon-m <tkksm2@gmail.com>
2025-05-14 23:05:45 +0900

    chore: langsmith tracing

--- src/index.py
+++ src/index.py
@@ -3,6 +3,7 @@ from langchain_openai import ChatOpenAI
 
 import os
 
+
 def main():
     load_dotenv()
 
@@ -11,10 +12,9 @@ def main():
     print(f"OPENAI_API_KEY={openai_api_key}")
 
     llm = ChatOpenAI(
-        model="gpt-4o-mini",
-        openai_api_key=openai_api_key
+        temperature=0.1,  # 창의성 (0.0 ~ 2.0)
+        model="gpt-4o",
     )
-
 
     print(f"[답변]: {llm.invoke('대한민국의 수도는 어디인가요?')}")
 
