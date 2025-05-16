3d53b703c0ade8e1b77b224a8cb9599d57bcd7f0
Ssoon-m <tkksm2@gmail.com>
2025-05-16 22:03:24 +0900

    StrOutputParser

--- src/lcel.py
+++ src/lcel.py
@@ -1,23 +1,56 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
-
-import os
+from langchain_core.output_parsers import StrOutputParser
 
 
 def main():
     load_dotenv()
 
     prompt = PromptTemplate.from_template("{topic} 에 대해 {how} 설명해주세요.")
-
     model = ChatOpenAI(model="gpt-4.1-nano", temperature=0.1)
+    parser = StrOutputParser()
 
-    chain = prompt | model
+    chain = prompt | model | parser
 
     input = {"topic": "인공지능 모델의 학습 원리", "how": "영어로"}
 
-    chain.invoke(input)
+    answer = chain.invoke(input)
+
+    print(answer)
+
+
+def output_parser_test():
+    load_dotenv()
+
+    template = """
+    당신은 영어를 가르치는 10년차 영어 선생님입니다. 주어진 상황에 맞는 영어 회화를 작성해 주세요.
+    양식은 [FORMAT]을 참고하여 작성해 주세요.
+
+    #상황:
+    {question}
+
+    #FORMAT:
+    - 영어 회화:
+    - 한글 해석:
+    """
+
+    # 프롬프트 템플릿을 이용하여 프롬프트를 생성합니다.
+    prompt = PromptTemplate.from_template(template)
+
+    # ChatOpenAI 챗모델을 초기화합니다.
+    model = ChatOpenAI(model_name="gpt-4-turbo")
+    parser = StrOutputParser()
+
+    chain = prompt | model | parser
+
+    input = {"question": "저는 식당에 가서 음식을 주문하고 싶어요"}
+
+    answer = chain.stream(input)
+
+    for chunk in answer:
+        print(chunk, end="", flush=True)
 
 
 if __name__ == "__main__":
-    main()
+    output_parser_test()
