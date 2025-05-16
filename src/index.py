1989c6d673f20a0e9fd081cf63e82f4133be8dc5
Ssoon-m <tkksm2@gmail.com>
2025-05-16 21:02:18 +0900

    promptTemplate test

--- src/index.py
+++ src/index.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
 import os
 
@@ -7,16 +8,23 @@ import os
 def main():
     load_dotenv()
 
-    openai_api_key = os.getenv("OPENAI_API_KEY")
+    template = "{country}의 수도는 어디인가요?"
 
-    print(f"OPENAI_API_KEY={openai_api_key}")
+    prompt_template = ChatPromptTemplate.from_template(template)
+    prompt = prompt_template.format_prompt(country="대한민국")
 
-    llm = ChatOpenAI(
-        temperature=0.1,  # 창의성 (0.0 ~ 2.0)
-        model="gpt-4o",
-    )
+    print(prompt.to_string())
 
-    print(f"[답변]: {llm.invoke('대한민국의 수도는 어디인가요?')}")
+    # openai_api_key = os.getenv("OPENAI_API_KEY")
+
+    # print(f"OPENAI_API_KEY={openai_api_key}")
+
+    # llm = ChatOpenAI(
+    #     temperature=0.1,  # 창의성 (0.0 ~ 2.0)
+    #     model="gpt-4o",
+    # )
+
+    # print(f"[답변]: {llm.invoke('대한민국의 수도는 어디인가요?')}")
 
 
 if __name__ == "__main__":
